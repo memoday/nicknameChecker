03779c056322ae9eca61a504c2edb5fef1f65f62
memoday <74040890+memoday@users.noreply.github.com>
2022-11-08 17:39:42 +0900

    Update nicknameChecker.py
    version description added

--- nicknameChecker.py
+++ nicknameChecker.py
@@ -8,6 +8,8 @@ import requests
 import openpyxl
 import time
 
+__version__ = '1.1.0'
+
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
